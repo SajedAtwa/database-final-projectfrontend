0b2d060300eb897dc0623331f118dcd04d09ab83
Pretam07 <Chowdhurypretam@gmail.com>
2024-04-01 15:03:07 -0400

    added links

--- database_frontend/src/components/CategoryList.js
+++ database_frontend/src/components/CategoryList.js
@@ -3,25 +3,55 @@ import '../static/css/CategoryList.css';
 import image from '../static/images/screen_replace.png';
 import image2 from '../static/images/battery.png';
 import image3 from '../static/images/water_damage.png';
+import { Link } from 'react-router-dom';
 
 function CategoryList() {
   const categories = [
-    { name: 'Screen Replacement', icon: image },
-    { name: 'Battery Replacement', icon: image2 },
-    { name: 'Water Damage Repair', icon: image3 },
-    { name: 'Software Troubleshooting', icon: '/path-to-icons/software-troubleshooting.svg' },
-    { name: 'Hardware Upgrades', icon: '/path-to-icons/hardware-upgrades.svg' },
-    { name: 'Data Recovery', icon: '/path-to-icons/data-recovery.svg' },
-    { name: 'Charging Port Repair', icon: '/path-to-icons/charging-port-repair.svg' },
-    { name: 'Camera Repair', icon: '/path-to-icons/camera-repair.svg' }
+    { 
+      name: 'Screen Replacement', 
+      icon: image,  
+      linkName: '/repairservice'
+    },
+    { name: 
+      'Battery Replacement', 
+      icon: image2,
+      linkName: '/repairservice'
+    },
+    { name: 'Water Damage Repair',
+     icon: image3,
+     linkName: '/repairservice'
+    },
+    { 
+      name: 'Software Troubleshooting', 
+      icon: '/path-to-icons/software-troubleshooting.svg',
+      linkName: '/repairservice'
+    },
+    { name: 'Hardware Upgrades', 
+    icon: '/path-to-icons/hardware-upgrades.svg',
+    linkName: '/repairservice' 
+  },
+    { name: 'Data Recovery', 
+    icon: '/path-to-icons/data-recovery.svg',
+    linkName: '/repairservice'
+  },
+    { name: 'Charging Port Repair', 
+    icon: '/path-to-icons/charging-port-repair.svg',
+    linkName: '/repairservice'
+  },
+    { name: 'Camera Repair', 
+    icon: '/path-to-icons/camera-repair.svg',
+    linkName: '/repairservice' 
+  }
   ];
 
   return (
     <div className="category-list">
       {categories.map((category, index) => (
         <div key={index} className="category">
+          <Link to={category.linkName} className=''> 
           <img src={category.icon} alt={`${category.name} Icon`} />
           <span>{category.name}</span>
+          </Link>
         </div>
       ))}
     </div>
