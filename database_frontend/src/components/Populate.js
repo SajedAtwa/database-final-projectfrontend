2ccbcf2a893bb95da0435abc5049bb94ef743179
NTRayne <nazifa.talukder2425@gmail.com>
2024-04-21 19:31:18 -0400

    edits to populate and drop

--- database_frontend/src/components/Populate.js
+++ database_frontend/src/components/Populate.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { populateDatabase } from '../db methods/dbAdmin';
+import * as User from "../Users.js";
 
 const Populate = () => {
   const history = useHistory();
 
   useEffect(() => {
-    //sessionStorage.clear();
     const populate = async () => {
       
-      const uidString = sessionStorage.getItem('uid');
-      const uid = parseInt(uidString, 10);
-      const password = sessionStorage.getItem('password'); 
+      const uid = User.getUser('uid');
+      //const uid = parseInt(uidString, 10);
+      const password = User.getUser('password'); 
 
       await populateDatabase(uid, password);
       history.push('/dashboard');
