544c94067651490d51359b309439f450dff78474
SajedAtwa <sajedatwa78@gmail.com>
2024-04-24 20:15:26 -0400

    added icons
    Fixed icons for info section and popular categories

--- database_frontend/src/components/CategoryList.js
+++ database_frontend/src/components/CategoryList.js
@@ -2,10 +2,12 @@ import React from 'react';
 import '../static/css/CategoryList.css';
 import image from '../static/images/screen.png';
 import image2 from '../static/images/battery.png';
-import image3 from '../static/images/water_damage.png';
-import image4 from '../static/images/camera.png';
-import image5 from '../static/images/keyboard.png';
+import image3 from '../static/images/water_damages.png';
+import image4 from '../static/images/cameras.png';
+import image5 from '../static/images/keyboards.png';
 import image6 from '../static/images/charging_port.png';
+import image7 from '../static/images/ipad-repair.png';
+import image8 from '../static/images/laptop-repair.png';
 
 function CategoryList() {
   const categories = [
@@ -13,8 +15,8 @@ function CategoryList() {
     { name: 'Battery Replacement', icon: image2 },
     { name: 'Water Damage Repair', icon: image3 },
     { name: 'Keyboard Repair', icon: image5 },
-    { name: 'Tablet Repair', icon: '/path-to-icons/hardware-upgrades.svg' },
-    { name: 'Laptop Repair', icon: '/path-to-icons/data-recovery.svg' },
+    { name: 'Tablet Repair', icon: image7 },
+    { name: 'Laptop Repair', icon: image8 },
     { name: 'Charging Port Repair', icon: image6 },
     { name: 'Camera Repair', icon: image4 }
   ];
